753dc1ed80b284353f3f3d5a6ab3e1467b363a19
Maikol Moreira <Niukeitor@hotmail.com>
2021-05-06 20:03:30 +0000

    todo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center mt-5 col-4">
+			<h1>Todos</h1>
+
+			<ul className="list-group">
+				<li className="list-group-item">
+					<form onSubmit={}>
+						<input type="text"></input>
+					</form>
+				</li>
+				<li className="list-group-item"></li>
+				<li className="list-group-item"></li>
+				<li className="list-group-item"></li>
+				<li className="list-group-item"></li>
+			</ul>
 		</div>
 	);
 }
