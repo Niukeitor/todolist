2963c9bc23727930be011d5b27313f6424b8f62f
Maikol Moreira <Niukeitor@hotmail.com>
2021-05-07 18:06:42 +0000

    list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 export function Home() {
+	const [lista, setLista] = useState([]);
+
+	const [tarea, setTarea] = useState("");
+
+	const handleInput = evento => {
+		if (evento.charCode === 13 && evento.target.value !== "") {
+			let nuevaTarea = {
+				label: evento.target.value,
+				done: false
+			};
+			let newList = [...lista, nuevaTarea];
+			console.log(newList);
+			setLista(newList);
+			evento.target.value = "";
+		}
+	};
+
 	return (
 		<div className="container text-center mt-5 col-4">
 			<h1>Todos</h1>
-
+			<input placeholder="agregar tarea" onKeyPress={handleInput} />
 			<ul className="list-group">
-				<li className="list-group-item">
-					<form onSubmit={}>
-						<input type="text"></input>
-					</form>
-				</li>
-				<li className="list-group-item"></li>
-				<li className="list-group-item"></li>
-				<li className="list-group-item"></li>
-				<li className="list-group-item"></li>
+				{lista.map((elemento, index, array) => {
+					return (
+						<li key={index} className="list-group-item">
+							{elemento.label}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
